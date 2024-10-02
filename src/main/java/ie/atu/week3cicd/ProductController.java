0ada7c0449f867165ecea4f19d2a85970eef2839
G00413711@atu.ie <G00413711@atu.ie>
2024-10-02 14:44:01 +0100

    Created methods to get products and add products

--- src/main/java/ie/atu/week3cicd/ProductController.java
+++ src/main/java/ie/atu/week3cicd/ProductController.java
@@ -1,13 +1,32 @@
 package ie.atu.week3cicd;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class ProductController {
-    @GetMapping
-    public String getProducts()
+
+    private List<Product> products = new ArrayList<>();
+    public ProductController()
     {
-        return "products";
+        products.add(new Product("Tv","Made by Sony",899,100));
+        products.add(new Product("Radio","Made by Samsung",120,101));
+    }
+    @GetMapping("/getProducts")
+    public List<Product> getProducts()
+    {
+        return products;
+    }
+    @PostMapping("addProduct")
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
+        products.add(product);
+        return ResponseEntity.ok(products);
     }
 }
